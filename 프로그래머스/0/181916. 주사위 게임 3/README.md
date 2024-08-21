d176f76161dfa2ebd00ccb0586b4db8ae53affdb
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2024-08-21 10:47:19 +0900

    [level 0] Title: 주사위 게임 3, Time: 0.58 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/README.md
+++ 프로그래머스/0/181916. 주사위 게임 3/README.md
@@ -1,10 +1,10 @@
 # [level 0] 주사위 게임 3 - 181916 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181916#) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 0.79 ms
+메모리: 73.7 MB, 시간: 0.58 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 08월 21일 10:21:48
+2024년 08월 21일 10:47:18
 
 ### 문제 설명
 
--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -1,61 +1,45 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int a, int b, int c, int d) {
         int answer = 0;
-        int[] cnt = new int[7];
-        cnt[a]++;
-        cnt[b]++;
-        cnt[c]++;
-        cnt[d]++;
-        for(int i = 1; i<cnt.length; i++){
-            if(cnt[i] == 4){
-                answer = i*1111;
-                break;
-            }
-            if(cnt[i] == 3){
-                for(int j = 1; j<cnt.length; j++){
-                    if(cnt[j] == 1){
-                        answer = (int)Math.pow(10*i + j,2);
-                        break;
-                    }
-                }
-            }
-            if(cnt[i] == 2){
-                for(int j = 1; j<cnt.length; j++){
-                    if(cnt[j] == 2){
-                        answer = (i+j) * Math.abs(i-j);
-                        break;
-                    }
-                }
-            }
-            if(cnt[i] == 2){
-                for(int j = 1; j<cnt.length; j++){
-                    if(cnt[j] == 1){
-                        for(int k = j+1; k<cnt.length; k++){
-                            if(cnt[k] == 1){
-                                answer = j*k;
-                                break;
-                            }
-                        }
-                    }
-                }
-            }
-            if(cnt[i] == 1){
-                for(int j = i+1; j<cnt.length; j++){
-                    if(cnt[j] == 1){
-                        for(int k = j+1; k<cnt.length; k++){
-                            if(cnt[k] == 1){
-                                for(int l = k+1; l<cnt.length; l++){
-                                    if(cnt[l] == 1){
-                                        answer = i;
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
+        int[] dice = {a, b, c, d};
+        Arrays.sort(dice);
+        
+        if(dice[0] == dice[3]){
+            answer = 1111*dice[0];
+        }
+        else if(dice[0]==dice[2]){
+            answer = threeDice(dice[0], dice[3]);
+        }
+        else if(dice[1]==dice[3]){
+            answer = threeDice(dice[1], dice[0]);
+        }
+        else if(dice[0]==dice[1] && dice[2]==dice[3]){
+            answer = doubleDice(dice[0], dice[2]);
+        }
+        else if(dice[0]==dice[1]){
+            answer = twoDice(dice[2], dice[3]);
+        }
+        else if(dice[1]==dice[2]){
+            answer = twoDice(dice[0], dice[3]);
+        }
+        else if(dice[2]==dice[3]){
+            answer = twoDice(dice[0], dice[1]);
+        }
+        else{
+            answer = dice[0];
         }
         return answer;
+    }
+        
+    public int threeDice(int p, int q){
+        return (int)Math.pow(10*p + q, 2);
+    }    
+    public int doubleDice(int p, int q){
+        return (p+q) * Math.abs(p-q);
+    }    
+    public int twoDice(int p, int q){
+        return p*q;
     }
 }

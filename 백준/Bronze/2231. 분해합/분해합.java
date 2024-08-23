da045dec869169c265e1721d085a6556fef39c5b
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2024-08-23 17:12:14 +0900

    [Bronze II] Title: 분해합, Time: 168 ms, Memory: 17688 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.java
+++ 백준/Bronze/2231. 분해합/분해합.java
@@ -4,19 +4,24 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
+		String n = String.valueOf(N);
 		int ans = 0;
 		int sum = 0;
-		for(int i = 1; i<N; i++) {
-			String num = String.valueOf(i);
+		int start = N-9*n.length();
+		if(start<1) {
+			start = 1;
+		}
+		for (int i = start; i < N; i++) {
+			String strStart = String.valueOf(i);
 			sum = i;
 			if(sum !=N) {
-				for (int j = 0; j < num.length(); j++) {
-					sum += num.charAt(j)-'0';
+				for (int j = 0; j < strStart.length(); j++) {
+					sum += strStart.charAt(j) -'0';
 				}
 			}
-			if (sum == N) {
+			if(sum == N) {
 				ans = i;
-				break;	
+				break;
 			}
 		}
 		System.out.println(ans);

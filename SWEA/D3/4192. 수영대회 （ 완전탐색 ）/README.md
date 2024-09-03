830c7bced393d6d4650590d8ceee59bd0d257fb4
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2024-09-03 12:20:48 +0900

    [D3] Title: 수영대회 ( 완전탐색 ), Time: 140 ms, Memory: 20,000 KB -BaekjoonHub

---
+++ SWEA/D3/4192. 수영대회 （ 완전탐색 ）/README.md
@@ -0,0 +1,15 @@
+# [D3] 수영대회 ( 완전탐색 ) - 4192 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWKaCc-KABgDFAT2) 
+
+### 성능 요약
+
+메모리: 20,000 KB, 시간: 140 ms, 코드길이: 2,048 Bytes
+
+### 제출 일자
+
+2024-09-03 12:20
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/4192. 수영대회 （ 완전탐색 ）/수영대회 （ 완전탐색 ）.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int T;
+	static int N;
+	static int A;
+	static int B;
+	static int C;
+	static int D;
+	
+	static int dis;
+	
+	static int[][] map;
+	static int[][] visited;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		T = Integer.parseInt(br.readLine());
+		
+		for (int tc = 1; tc < T+1; tc++) {
+			N = Integer.parseInt(br.readLine());
+			
+			map = new int[N][N];
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			
+			//start
+			A = Integer.parseInt(st.nextToken());
+			B = Integer.parseInt(st.nextToken());
+			
+			st = new StringTokenizer(br.readLine());
+			
+			//end
+			C = Integer.parseInt(st.nextToken());
+			D = Integer.parseInt(st.nextToken());
+			
+			visited = new int[N][N];
+			
+			bfs(A,B);
+			
+			StringBuilder sb = new StringBuilder();
+			sb.append("#").append(tc).append(" ").append(dis);
+			
+			System.out.println(sb.toString());
+		}
+	}
+
+	static int[] dr = {-1,0,1,0};
+	static int[] dc = {0,1,0,-1};
+	
+	static void bfs(int sr, int sc) {
+		Queue<int[]> que = new LinkedList<>();
+		que.offer(new int[] {sr,sc});
+		dis = -1;
+		visited[sr][sc] = 1;
+		while(!que.isEmpty()) {
+			int[] cur = que.poll();
+			int r = cur[0];
+			int c = cur[1];
+			if(r==C && c==D) {
+				dis = visited[r][c] -1;
+				return;
+			}
+			for(int d = 0; d<4;d++) {
+				int nr = r+dr[d];
+				int nc = c+dc[d];
+				if(!check(nr,nc))continue;
+				if(visited[nr][nc]>0) continue;
+				if(map[nr][nc] == 0) {
+					visited[nr][nc] = visited[r][c]+1;
+					que.offer(new int[]{nr, nc});
+				}
+			}
+		}
+	}
+	
+	static boolean check(int r, int c) {
+		return r>=0 && r<N && c>=0 && c<N;
+	}
+}

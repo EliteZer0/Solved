be3b11fe80026b10379cb8848e94f64e35a5aad6
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2024-09-04 17:26:50 +0900

    [Silver III] Title: N과 M (2), Time: 100 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int r;
+	static int[] nums;
+	static int[] choice;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		nums = new int[n];
+		
+		for (int i = 0; i < n; i++) {
+			nums[i] = i+1;
+		}
+		
+		choice = new int[r];
+		combi(0, 0);
+	}
+
+	static void combi(int start, int idx) {
+		StringBuilder sb = new StringBuilder();
+		
+		if(idx == r) {
+			for (int i = 0; i < r; i++) {
+				sb.append(choice[i]).append(" ");
+			}
+			System.out.println(sb.toString());
+			return;
+		}
+		
+		for (int i = start; i < n; i++) {
+			choice[idx] = nums[i];
+			combi(i+1, idx+1);
+		}
+	}
+}

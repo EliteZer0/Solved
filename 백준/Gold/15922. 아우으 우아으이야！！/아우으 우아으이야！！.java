a36d5874c1e852fcf3ea9e2b15d9fdbef23184ac
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2024-10-30 00:02:22 +0900

    [Gold V] Title: 아우으 우아으이야!!, Time: 308 ms, Memory: 41196 KB -BaekjoonHub

--- 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.java
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.java
@@ -12,65 +12,45 @@ import java.util.StringTokenizer;
 //이렇게 진행하니 수의 범위가 커버가 안됨.
 //선분의 시작점과 끝점을 받는 클래스를 트리셋으로 받아서 진행.
 //트리셋이 생각했던 것만큼 빠르게 진행이 안 돼서 리스트로 바꿔서 진행
-//메모리 3000 시간 50 정도 감소
-class Segment implements Comparable<Segment> {
-    int start, end;
-
-    public Segment(int start, int end) {
-        this.start = start;
-        this.end = end;
-    }
-
-    @Override
-    public int compareTo(Segment o) {
-        if (this.start == o.start) {
-            return Integer.compare(this.end, o.end);
-        }
-        return Integer.compare(this.start, o.start);
-    }
-}
+//선분 병합 로직 자체를 단순화 해봄
 
 public class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		List<Segment> segments = new ArrayList<>();
 		StringTokenizer st;
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		for (int i = 0; i < n; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
-			int end = Integer.parseInt(st.nextToken());
-			segments.add(new Segment(start, end));
-		}
-		
-		//Collections.sort(segments);
-		//정렬할 필요가 없음. 애초에 정렬을 해서 준다고 문제에 적혀있음.
-		//시작점 설정
-		int start = segments.get(0).start;
-		int end = segments.get(0).end;
+		int start = Integer.MIN_VALUE;
+		int end = Integer.MIN_VALUE;
 		int length = 0;
 		
-		for(Segment s : segments) {
+		for (int i = 0; i < n; i++) {
+			st = new StringTokenizer(br.readLine());
+			int curStart = Integer.parseInt(st.nextToken());
+			int curEnd = Integer.parseInt(st.nextToken());
+			
 			//새로 뽑은 선분과 이전 선분의 범위가 겹치면
-			if(s.start <= end) {
+			if(curStart <= end) {
 				//이전 선분 끝나는 지점과 새로 뽑은 선분 끝나는 지점 중에서 더 큰 거
-				end = Math.max(end, s.end);
+				end = Math.max(end, curEnd);
 			} 
 			//겹치지 않으면
 			else {
-				//현재까지 선분 길이 총 길이에 합하고
-				length += end-start;
+				if(start != Integer.MIN_VALUE) {
+					length += end-start;
+				}
 				//시작 위치 새로 뽑은 선분의 시작위치로 갱신
-				start = s.start;
+				start = curStart;
 				//끝나는 위치 새로 뽑은 선분의 끝나는 위치로 갱신
-				end = s.end;
+				end = curEnd;
 			}
 		}
 		
 		//마지막 선분 길이까지 더해주기
-		length += end-start;
+		if(start != Integer.MIN_VALUE) {
+			length += end-start;
+		}
 		
 		System.out.println(length);
 	} 

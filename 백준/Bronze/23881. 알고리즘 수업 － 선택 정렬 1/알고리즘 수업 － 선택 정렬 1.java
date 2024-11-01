ab0b8269b02c07ec670e79cf1c08444e76d5ce82
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2024-11-01 10:01:11 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 1, Time: 240 ms, Memory: 14956 KB -BaekjoonHub

--- 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.java
+++ 백준/Bronze/23881. 알고리즘 수업 － 선택 정렬 1/알고리즘 수업 － 선택 정렬 1.java
@@ -1,14 +1,19 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int k = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
         int[] data = new int[n];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            data[i] = sc.nextInt();
+            data[i] = Integer.parseInt(st.nextToken());
         }
 
         int count = 0;

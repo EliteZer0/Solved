4e7023e840d82409ef8f2312b085e7fc696f29f7
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2025-06-18 12:29:47 +0900

    [Gold II] Title: 가운데를 말해요, Time: 368 ms, Memory: 34900 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -1,39 +1,40 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
         // 최대 힙 (중간값 이하)
         PriorityQueue<Integer> left = new PriorityQueue<>(Collections.reverseOrder());
-
         // 최소 힙 (중간값 초과)
         PriorityQueue<Integer> right = new PriorityQueue<>();
 
         StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            int num = sc.nextInt();
+            int num = Integer.parseInt(br.readLine());
 
-            // 일단 왼쪽 최대 힙에 삽입
+            // 일단 최대 힙에 넣기
             left.offer(num);
 
-            // 최대 힙의 top > 최소 힙의 top이면 교환 (중간값 조건 유지)
+            // 최대 힙의 최대값이 최소 힙의 최소값보다 크면 swap
             if (!right.isEmpty() && left.peek() > right.peek()) {
                 right.offer(left.poll());
                 left.offer(right.poll());
             }
 
-            // left의 크기 > right보다 1 이상 크면 right로 이동
+            // left가 right보다 두 개 이상 크면 균형 맞추기
             if (left.size() > right.size() + 1) {
                 right.offer(left.poll());
             }
 
-            // 중간값 출력
+            // 중간값은 항상 left의 top
             sb.append(left.peek()).append("\n");
         }
 
-        System.out.print(sb);
+        // 결과 출력
+        System.out.print(sb.toString());
     }
 }

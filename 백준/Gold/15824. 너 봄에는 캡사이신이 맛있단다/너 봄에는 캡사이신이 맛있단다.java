2e08f06d9d8b28b1814bfe1b5992d7c9c13076a1
Diana Yeyoung Jeong <Diana.yy.Jeong@gmail.com>
2025-11-11 21:50:34 +0900

    [Gold II] Title: 너 봄에는 캡사이신이 맛있단다, Time: 612 ms, Memory: 66136 KB, Score: 250 point -BaekjoonHub

--- 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.java
+++ 백준/Gold/15824. 너 봄에는 캡사이신이 맛있단다/너 봄에는 캡사이신이 맛있단다.java
@@ -4,7 +4,7 @@ import java.io.*;
 
 class Main {
     final static int MOD = 1_000_000_007;
-    
+    //템플릿
     public static long powMod(long base, long exp) {
         long result = 1;
         base %= MOD;
@@ -33,12 +33,25 @@ class Main {
         Arrays.sort(arr);
 
         long sum = 0;
-        
-        for(int i = 0; i<N-1; i++){
-            for(int j = N-1; j>i; j--){
-                long cal = ((arr[j]-arr[i]) * powMod(2, j-i-1)) % MOD;
-                sum = (sum+cal) % MOD;
-            }
+
+        // LARGE 조건에서 O(N^2)이라 시간초과 나는듯
+        // for(int i = 0; i<N-1; i++){
+        //     for(int j = N-1; j>i; j--){
+        //         long cal = ((arr[j]-arr[i]) * powMod(2, j-i-1)) % MOD;
+        //         sum = (sum+cal) % MOD;
+        //     }
+        // }
+
+        for(int i = 0; i < N; i++){
+            // arr[i]가 최댓값이 되는 경우
+            // 자신과 자신보다 작은 원소들로 구성 : 2^i개
+            long maxContribution = (arr[i] % MOD * powMod(2, i)) % MOD;
+            sum = (sum + maxContribution) % MOD;
+            
+            // arr[i]가 최솟값이 되는 경우
+            // 자신과 자신보다 작은 원소들로 구성 : 2^(N-1-i)개
+            long minContribution = (arr[i] % MOD * powMod(2, N - 1 - i)) % MOD;
+            sum = (sum - minContribution + MOD) % MOD;
         }
         
         System.out.println(sum);
